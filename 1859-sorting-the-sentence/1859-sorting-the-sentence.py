398b011f25c98f26b5e1b20f081b64fc0118d37b
Jennifer Lim <73134949+limjen427@users.noreply.github.com>
2022-10-07 19:03:11 -0500

    Time: 58 ms (31.80%), Space: 13.8 MB (59.12%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,11 +1,11 @@
 class Solution:
     def sortSentence(self, s: str) -> str:
-        dict_t = {}
-        ans = []
+        dic = {}
+        result = []
         for word in s.split():
             num = int(word[-1])
             val = word[:-1]
-            dict_t[num] = val
-        for key in sorted(dict_t.keys()):
-            ans.append(dict_t[key])
-        return " ".join(ans)
+            dic[num] = val
+        for key in sorted(dic.keys()):
+            result.append(dic[key])
+        return " ".join(result)
