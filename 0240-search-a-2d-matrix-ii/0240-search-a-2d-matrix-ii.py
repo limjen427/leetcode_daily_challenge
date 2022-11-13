aaa9ef2105eed14cccc177695bf5210918f39128
Jennifer Lim <73134949+limjen427@users.noreply.github.com>
2022-11-13 15:36:01 -0600

    Time: 403 ms (46.53%), Space: 20.3 MB (83.72%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,19 +1,38 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        #binary search in each row
-        #linear in each column
+#     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+#         #binary search in each column
+#         #linear in each row
         
-        for r in range(len(matrix)):
-            low = 0
-            high = len(matrix[r]) - 1
-            print(r, low, high)
+#         for r in range(len(matrix)):
+#             low = 0
+#             high = len(matrix[r]) - 1
             
-            while (low <= high):
-                mid = (low + high) // 2
-                if matrix[r][mid] == target:
+#             while (low <= high):
+#                 mid = (low + high) // 2
+#                 if matrix[r][mid] == target:
+#                     return True
+#                 elif matrix[r][mid] > target:
+#                     high = mid - 1
+#                 else:
+#                     low = mid + 1
+#         return False
+#     #runtime O(mlogn)
+    
+    #runtime O(n+m)
+     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+            # compare target with matrix[row][col]?????
+            # search space reduce
+            m = len(matrix)
+            n = len(matrix[0])
+            
+            row = 0
+            col = n - 1
+            while (row < m and col >= 0):
+                if matrix[row][col] == target:
                     return True
-                elif matrix[r][mid] > target:
-                    high = mid - 1
+                elif matrix[row][col] < target:
+                    row += 1
                 else:
-                    low = mid + 1
-        return False
+                    col -= 1
+            
+            return False
